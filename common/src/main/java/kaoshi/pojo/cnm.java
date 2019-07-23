d23caf2b20daaf85dab9cb60ef1f04e404994145
lzy <1064177869@qq.com>
2019-07-23 15:39:48 +0800

    lzy再一次更改2

--- common/src/main/java/kaoshi/pojo/cnm.java
+++ common/src/main/java/kaoshi/pojo/cnm.java
@@ -1,4 +1,13 @@
 package kaoshi.pojo;
 
 public class cnm {
+    private String word;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
 }
